e2b210e87143070e480eb71836da1fa687f074d7
André Vargas <vargas@geosapiens.com.br>
2016-11-07 16:58:06 -0200

    Adicionado SRID.

--- src/main/java/br/univali/sisnet/util/GeometryUtil.java
+++ src/main/java/br/univali/sisnet/util/GeometryUtil.java
@@ -4,11 +4,13 @@ package br.univali.sisnet.util;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
 
 
 public class GeometryUtil {
 
-    private static final GeometryFactory geometryFactory = new GeometryFactory();
+    private static final GeometryFactory geometryFactory =
+            new GeometryFactory(new PrecisionModel(), 4674);
 
     public static Point latLongToPoint (Float latitude, Float longitude) {
 
